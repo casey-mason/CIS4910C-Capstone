c528077d8915c616fcee4654ffcc9011dde1b6d2
casey-mason <44148892+casey-mason@users.noreply.github.com>
2019-03-03 16:38:05 -0600

    Module 4 Project

---
+++ CIS4910C/Insert.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO cmtracker (id, CustomerName, Username, Password, LoanDebt, TotalLoans)
+VALUES ('3456', 'Adam Koop', 'akoop', 'versace24', '1000', '1500'), ('4567', 'Kari Deters', 'kdeters', 'suzy0879!', '2000', '5500');
